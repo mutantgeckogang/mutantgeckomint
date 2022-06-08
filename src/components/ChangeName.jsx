cccae6f6081da08136e8008b73f7df902e38af67
ryxns <33070164+ryxns@users.noreply.github.com>
2022-06-07 23:32:32 -0700

    Update ChangeName.jsx

--- src/components/ChangeName.jsx
+++ src/components/ChangeName.jsx
@@ -10,7 +10,7 @@ export default function ChangeName(props) {
         onClick={() => {
           changeName(name);
         }}
-        className="bg-green-500 px-6 py-2 rounded-sm text-xs font-semibold uppercase text-white cursor-pointer"
+        className="bg-green-800 px-6 py-2 rounded-sm text-xs font-semibold uppercase text-white cursor-pointer"
       >
         MINT
       </button>
