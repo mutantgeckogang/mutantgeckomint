f8804bcd4c296c5bbeb2679c56c222ef67ea29ba
ryxns <33070164+ryxns@users.noreply.github.com>
2022-06-07 21:45:27 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ const App = () => {
     <div className="bg-black min-h-screen">
       <Navbar />
       <div className="container pt-10 mx-auto">
-        <img src="https://mutantgeckogang.xyz/assets/index.png">
+        <img src="https://mutantgeckogang.xyz/assets/index.png" />
         <div className="flex justify-center">
           <h1 className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-500 via-red-300 to-green-400">
             Hello, Mutant Gecko Gang!
           </h1><br>
-          <img src="https://mutantgeckogang.xyz/assets/index.png">
+          <img src="https://mutantgeckogang.xyz/assets/index.png" />
         </div>
         <div className="mt-20 flex justify-center">
           <ChangeName />
