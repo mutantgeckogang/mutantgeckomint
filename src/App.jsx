d87998a7b4f00e88fdf652698e35e8db16e75955
ryxns <33070164+ryxns@users.noreply.github.com>
2022-06-07 21:12:26 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import ChangeName from "./components/ChangeName";
 
 const App = () => {
   return (
-    <div className="bg-black min-h-screen" style={myStyle} >
+    <div className="bg-black min-h-screen">
       <Navbar />
-      <div className="container pt-10 mx-auto" style={myStyle} >
-        <div className="flex justify-center" style={myStyle} >
+      <div className="container pt-10 mx-auto">
+        <div className="flex justify-center">
           <h1 className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-500 via-red-300 to-green-400">
             Hello, Mutant Gecko Gang!
           </h1>
         </div>
-        <div className="mt-20 flex justify-center" style={myStyle} >
+        <div className="mt-20 flex justify-center">
           <ChangeName />
         </div>
       </div>
