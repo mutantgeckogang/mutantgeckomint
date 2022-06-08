5aba497b80098fa1d56087ad5b6d30e535670cab
ryxns <33070164+ryxns@users.noreply.github.com>
2022-06-07 20:41:49 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Navbar from "./components/Navbar";
 import ChangeName from "./components/ChangeName";
-import bg from './images/mint.png'
+import bg1 from './mint.png'
 
 const App = () => {
   return (
     <div className="bg-black min-h-screen">
       <Navbar />
-      <div className="container pt-10 mx-auto" styles={{ backgroundImage:`url(${bg})` }}>
+      <div className="container pt-10 mx-auto" styles={{ backgroundImage:`url(${bg1})` }}>
         <div className="flex justify-center">
           <h1 className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-500 via-blue-300 to-red-400">
             Hello, Tezos!
