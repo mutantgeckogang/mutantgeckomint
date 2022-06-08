e4c6d927c493ac2b7ff752b487be520728d2f02c
ryxns <33070164+ryxns@users.noreply.github.com>
2022-06-08 06:31:30 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div className="bg-black min-h-screen">
     <Navbar />
-    <center><img src="https://mutantgeckogang.xyz/assets/IMG_4299.PNG" width="512" /><ChangeName /></center>
+    <center><img src="https://mutantgeckogang.xyz/assets/IMG_4299.PNG" width="512" /></center>
     <div className="container pt-10 mx-auto">
         <div className="flex justify-center">
         </div>
