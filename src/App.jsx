61880820d5dce95b9259cc87bf237ffae32815f4
ryxns <33070164+ryxns@users.noreply.github.com>
2022-06-07 21:51:21 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,9 @@ import ChangeName from "./components/ChangeName";
 const App = () => {
   return (
     <div className="bg-black min-h-screen">
-    <img src="https://mutantgeckogang.xyz/assets/index.png" />  
     <Navbar />
-      <div className="container pt-10 mx-auto">
-        <img src="https://mutantgeckogang.xyz/assets/index.png" />
+    <img src="https://mutantgeckogang.xyz/assets/index.png" />
+    <div className="container pt-10 mx-auto">
         <div className="flex justify-center">
         </div>
         <div className="mt-20 flex justify-center">
