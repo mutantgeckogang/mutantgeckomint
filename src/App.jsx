c1c5ddf209e7de54b8ef34a1f9d3104b50243a43
mutantgeckogang <107126846+mutantgeckogang@users.noreply.github.com>
2022-06-09 00:38:21 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
         </div>
          <div className="mt-20 flex justify-center" >
            <h2 className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-500 via-green-300 to-green-500">
-            3136 / 4444
+            
           </h2>
         </div>
       </div>
