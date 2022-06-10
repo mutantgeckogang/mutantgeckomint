c587d968be18a50c8500f8bd05d0186070d67f48
mutantgeckogang <107126846+mutantgeckogang@users.noreply.github.com>
2022-06-10 00:40:24 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ const App = () => {
     <center><img src="https://mutantgeckogang.xyz/assets/sold_out.png" width="512" /></center>
     <div className="container pt-10 mx-auto">
         <div className="flex justify-center">
-    <ChangeName />
         </div>
          <div className="mt-20 flex justify-center" >
            <h3 className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-500 via-green-300 to-green-500">
