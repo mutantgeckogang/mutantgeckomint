0ebfada055e02be5b81a504e9c8753ed4e8e65b4
mutantgeckogang <107126846+mutantgeckogang@users.noreply.github.com>
2022-06-10 00:40:59 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
         </div>
          <div className="mt-20 flex justify-center" >
            <h3 className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-500 via-green-300 to-green-500">
-            SOLD OUT!
+            4444/4444
           </h3>
         </div>
       </div>
