c30c38b593109d6d0f883b6b08baa19fef97bb26
mutantgeckogang <107126846+mutantgeckogang@users.noreply.github.com>
2022-06-10 00:37:59 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ const App = () => {
   return (
     <div className="bg-black min-h-screen">
     <Navbar />
-    <center><img src="https://mutantgeckogang.xyz/assets/IMG_4299.PNG" width="512" /></center>
+    <center><img src="https://mutantgeckogang.xyz/assets/sold_out.png" width="512" /></center>
     <div className="container pt-10 mx-auto">
         <div className="flex justify-center">
     <ChangeName />
         </div>
          <div className="mt-20 flex justify-center" >
            <h3 className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-500 via-green-300 to-green-500">
-            4334/4444
+            SOLD OUT!
           </h3>
         </div>
       </div>
